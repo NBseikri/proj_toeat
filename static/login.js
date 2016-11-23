2cbf97c9f0d54a36cdd67e88a748ae5af29a18f1
vagrant <nbseikri@gmail.com>
2016-11-23 01:44:59 +0000

    Tag commit.

---
+++ static/login.js
@@ -0,0 +1,12 @@
+// FORM VALIDATION
+// function validateForm() {
+//    var x = document.forms["myForm"]["trip_name"].value;
+//    if (x == "") {
+//        alert("Fields must be filled out");
+//        return false;
+//    }
+// }
+// </script>
+
+
+// <form name="myForm" action="/createtrip" onsubmit="return validateForm()" method="post">
